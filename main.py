39bd4663a2558b17d128728e1966e6718f24fbad
shadow <zac.hong01@gmail.com>
2019-11-19 12:42:37 +0800

    add

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import re
+url = 'https://www.lazada.sg/products/acer-eb321hqu-d-315-inch-wqhd-ips-new-monitor-i444318328-s1181378318.html?spm=a2o42.searchlist.list.1.4f5a5c5bfVd3HS&search=1'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
+
+html = requests.get(url, headers=headers)
+print(html)
