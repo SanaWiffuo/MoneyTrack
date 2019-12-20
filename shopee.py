b888f05e0cbe8c62c676c6d92285b0e2483a00ed
hwen <whman63@gmail.com>
2019-12-21 02:47:39 +0800

    fake_useragent

--- shopee.py
+++ shopee.py
@@ -1,11 +1,17 @@
 import requests
 import pandas as pd
+from fake_useragent import UserAgent
+import time
+import random
 
+
+ua = UserAgent()
+userAgent = ua.random
 Shopee_url = 'https://shopee.sg'
-keyword_search = 'Monitor'
+keyword_search = input("Please enter the product name: ")
 headers = {
- 'User-Agent': 'Chrome',
- 'Referer': '{}search?keyword={}'.format(Shopee_url, keyword_search)
+'User-Agent': userAgent,
+'Referer': '{}search?keyword={}'.format(Shopee_url, keyword_search)
 }
 url = 'https://shopee.sg/api/v2/search_items/?by=relevancy&keyword={}&limit=100&newest=0&oanrder=desc&page_type=search'.format(keyword_search)
 
@@ -16,7 +22,8 @@ prices_list = []
 
 for item in r['items']:
     titles_list.append(item['name'])
-    prices_list.append(item['price_min'])
+    prices_list.append("$"+str(item['price_min']/100000))
 
 Shopee = pd.DataFrame(zip(titles_list, prices_list), columns=['ItemName', 'Price'])
 print(Shopee)
+
