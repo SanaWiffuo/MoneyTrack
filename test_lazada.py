a63d4e8c6fabf0975a1bad48e7ff00b10c37b5c3
hwen <whman63@gmail.com>
2019-12-21 02:48:07 +0800

    test

---
+++ test_lazada.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from fake_useragent import UserAgent
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import time
+import random as r
+
+file = "lines.txt"
+f = open(file)
+lst = []
+for i in f.read().splitlines():
+    lst.append(i)
+    
+
+while True:
+
+    opts = Options()
+    ua = UserAgent()
+    userAgent = ua.random
+    opts.add_argument(f'user-agenta={userAgent}')
+
+    webdriver_path= "/usr/local/bin/chromedriver"
+    Lazada_url = "https://www.lazada.sg"
+    search_item = lst[r.randint(1,18)]
+    total_of_result = r.randint(1,10)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless') 
+    options.add_argument('start-maximized') 
+    options.add_argument('disable-infobars')
+    options.add_argument('--disable-extensions')
+
+    browser = webdriver.Chrome(webdriver_path,options=options)
+    browser.get(Lazada_url)
+
+    search_bar = browser.find_element_by_id("q")
+    search_bar.send_keys(search_item)
+    search_bar.submit()
+
+    item_titles = browser.find_elements_by_class_name('c16H9d')
+    item_prices = browser.find_elements_by_class_name('c13VH6')
+
+
+    titles_list = []
+    prices_list = []
+
+    if len(item_titles)==0:
+        print("Fail")
+        
+    for i in range(total_of_result):
+        titles_list.append(item_titles[i].text)
+        prices_list.append(item_prices[i].text)
+    # for title in item_titles:
+    #     titles_list.append(title.text)
+    # for price in item_prices:
+    #     prices_list.append(price.text)
+
+    # print(titles_list)
+    # print(prices_list)
+
+    dfL = pd.DataFrame(zip(titles_list, prices_list), columns=['ItemName', 'Price'])
+    print(dfL)
+
+    browser.quit()
+    time.sleep(60)
+
+
