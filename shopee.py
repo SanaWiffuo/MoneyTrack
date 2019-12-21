9184b16de1c6ff9cf6255cf111797a8f03d0a074
hwen <whman63@gmail.com>
2019-12-21 09:59:19 +0800

    parameters

--- shopee.py
+++ shopee.py
@@ -4,26 +4,30 @@ from fake_useragent import UserAgent
 import time
 import random
 
+def scrap_shopee(keyword_search,total_of_result):
+    
+    ua = UserAgent()
+    userAgent = ua.random
+    Shopee_url = 'https://shopee.sg'
+    
+    headers = {
+    'User-Agent': userAgent,
+    'Referer': '{}search?keyword={}'.format(Shopee_url, keyword_search)
+    }
+    url = 'https://shopee.sg/api/v2/search_items/?by=relevancy&keyword={}&limit=100&newest=0&oanrder=desc&page_type=search'.format(keyword_search)
 
-ua = UserAgent()
-userAgent = ua.random
-Shopee_url = 'https://shopee.sg'
-keyword_search = input("Please enter the product name: ")
-headers = {
-'User-Agent': userAgent,
-'Referer': '{}search?keyword={}'.format(Shopee_url, keyword_search)
-}
-url = 'https://shopee.sg/api/v2/search_items/?by=relevancy&keyword={}&limit=100&newest=0&oanrder=desc&page_type=search'.format(keyword_search)
+    r = requests.get(url, headers = headers).json()
 
-r = requests.get(url, headers = headers).json()
+    titles_list = []
+    prices_list = []
 
-titles_list = []
-prices_list = []
+    for item in r['items']:
+        titles_list.append(item['name'])
+        prices_list.append("$"+str(item['price_min']/100000))
+        if len(titles_list)==total_of_result:
+            break
 
-for item in r['items']:
-    titles_list.append(item['name'])
-    prices_list.append("$"+str(item['price_min']/100000))
-
-Shopee = pd.DataFrame(zip(titles_list, prices_list), columns=['ItemName', 'Price'])
-print(Shopee)
-
+    # Shopee = pd.DataFrame(zip(titles_list, prices_list), columns=['ItemName', 'Price'])
+    # print(Shopee)
+    
+    return titles_list,prices_list
