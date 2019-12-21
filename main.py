58de0a9be90829f0003f5d115a2fd16937b30cd5
hwen <whman63@gmail.com>
2019-12-21 09:59:32 +0800

    parameters

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ import pandas as pd
 title_lst = []
 price_lst = []
 
-temp1,temp2 = scrap_lazada()
+search_item = input("Please enter the product name:")
+total_of_result = int(input("Please enter the number of result:"))
+
+temp1,temp2 = scrap_lazada(search_item,total_of_result)
 for i in range(len(temp1)):
     title_lst.append(temp1[i])
     price_lst.append(temp2[i])
 
-temp1,temp2 = scrap_shopee()
+temp1,temp2 = scrap_shopee(search_item,total_of_result)
 for i in range(len(temp1)):
     title_lst.append(temp1[i])
     price_lst.append(temp2[i])
 
     
 result = pd.DataFrame(zip(title_lst, price_lst), columns=['ItemName', 'Price'])
+print(result)
