9078a7bc223070b5d731651d22d480c6a6cad2c6
hwen <whman63@gmail.com>
2019-12-22 12:54:48 +0800

    classes

--- shopee.py
+++ shopee.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from fake_useragent import UserAgent
 import sys
+from classes import Product,Shopee
 
 def scrap_shopee(keyword_search,total_of_result):
     
@@ -18,15 +19,16 @@ def scrap_shopee(keyword_search,total_of_result):
     r = requests.get(url, headers = headers).json()
     
     
-    titles_list = []
-    prices_list = []
-    ratings_list = []
-    url_list = []
+    # titles_list = []
+    # prices_list = []
+    # ratings_list = []
+    # url_list = []
+    product_lst = []
     
     for item in r['items']:
-        titles_list.append(item['name'])
-        prices_list.append("$"+str(item['price_min']/100000))
-        ratings_list.append(item['item_rating'].get("rating_star"))
+        # titles_list.append(item['name'])
+        # prices_list.append("$"+str(item['price_min']/100000))
+        # ratings_list.append(item['item_rating'].get("rating_star"))
         
         url = "https://shopee.sg/"
         for i in item['name']: 
@@ -42,18 +44,19 @@ def scrap_shopee(keyword_search,total_of_result):
                     continue
                 url+="-"
                 
-        url_list.append(url+"-i.{}.{}".format(item['shopid'],item['itemid'])) #i wrote these based on the structure of the url by combining the name + shopid + itemid
-    print(url_list)
+        # url_list.append(url+"-i.{}.{}".format(item['shopid'],item['itemid'])) #i wrote these based on the structure of the url by combining the name + shopid + itemid
+        product_lst.append(Shopee(item['name'],"$"+str(item['price_min']/100000),item['item_rating'].get("rating_star"),url+"-i.{}.{}".format(item['shopid'],item['itemid'])))
+
     
-    if len(titles_list)==0:
+    
+    if len(product_lst)==0:
         print("Fail")
         sys.exit()
     
-    Shopee = pd.DataFrame(zip(titles_list, prices_list,ratings_list,url_list), columns=['ItemName', 'Price','Ratings',"Url"])
-    Shopee['Platform'] = 'Shopee'
-    print(Shopee)
-    print(url)
+   
+    df = pd.DataFrame([t.__dict__ for t in product_lst])
+    print(df)
     
-    return titles_list,prices_list,ratings_list,url_list
+    return product_lst
 
 scrap_shopee("food",10)
