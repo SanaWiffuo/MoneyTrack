bf0333f93b5828782bed5f34f9221931f629d58a
hwen <whman63@gmail.com>
2019-12-22 11:47:56 +0800

    scrape url

--- shopee.py
+++ shopee.py
@@ -15,17 +15,41 @@ def scrap_shopee(keyword_search,total_of_result):
     url = 'https://shopee.sg/api/v2/search_items/?by=relevancy&keyword={}&limit=100&newest=0&oanrder=desc&page_type=search'.format(keyword_search)
 
     r = requests.get(url, headers = headers).json()
-
+    
+    
     titles_list = []
     prices_list = []
-
+    ratings_list = []
+    url_list = []
+    
     for item in r['items']:
         titles_list.append(item['name'])
         prices_list.append("$"+str(item['price_min']/100000))
-        if len(titles_list)==total_of_result:
-            break
-
-    # Shopee = pd.DataFrame(zip(titles_list, prices_list), columns=['ItemName', 'Price'])
-    # print(Shopee)
+        ratings_list.append(item['item_rating'].get("rating_star"))
+        
+        url = "https://shopee.sg/"
+        for i in item['name']: 
+            
+            if i.isalpha() == True:
+                url+=i
+                
+            elif i.isdigit() == True:
+                url+=i
+                
+            else:
+                if url[-1] == "-":
+                    continue
+                url+="-"
+                
+        url_list.append(url+"-i.{}.{}".format(item['shopid'],item['itemid'])) #i wrote these based on the structure of the url by combining the name + shopid + itemid
+    print(url_list)
     
-    return titles_list,prices_list
+    
+    Shopee = pd.DataFrame(zip(titles_list, prices_list,ratings_list,url_list), columns=['ItemName', 'Price','Ratings',"Url"])
+    Shopee['Platform'] = 'Shopee'
+    print(Shopee)
+    print(url)
+    
+    return titles_list,prices_list,ratings_list,url_list
+
+scrap_shopee("food",10)
