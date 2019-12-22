d6d52bdfa38526f7389b512b6183fe53052bf997
hwen <whman63@gmail.com>
2019-12-22 13:09:42 +0800

    add

--- main.py
+++ main.py
@@ -2,23 +2,19 @@ from lazada import scrap_lazada
 from shopee import scrap_shopee
 import pandas as pd
 
-
-title_lst = []
-price_lst = []
+product_lst = []
 
 search_item = input("Please enter the product name:")
 total_of_result = int(input("Please enter the number of result:"))
 
-temp1,temp2 = scrap_lazada(search_item,total_of_result)
-for i in range(len(temp1)):
-    title_lst.append(temp1[i])
-    price_lst.append(temp2[i])
+temp1= scrap_lazada(search_item,total_of_result)
+for i in temp1:
+    product_lst.append(i)
 
-temp1,temp2 = scrap_shopee(search_item,total_of_result)
-for i in range(len(temp1)):
-    title_lst.append(temp1[i])
-    price_lst.append(temp2[i])
+temp1 = scrap_shopee(search_item,total_of_result)
+for i in temp1:
+    product_lst.append(i)
 
     
-result = pd.DataFrame(zip(title_lst, price_lst), columns=['ItemName', 'Price'])
-print(result)
+df = pd.DataFrame([t.__dict__ for t in product_lst])
+print(df)
