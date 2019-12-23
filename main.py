8b8270de6d1a9e4c33fd34549f9fdf83d608d3f5
hwen <whman63@gmail.com>
2019-12-23 11:21:07 +0800

    cleaner

--- main.py
+++ main.py
@@ -7,14 +7,8 @@ product_lst = []
 search_item = input("Please enter the product name:")
 total_of_result = int(input("Please enter the number of result:"))
 
-temp1= scrap_lazada(search_item,total_of_result)
-for i in temp1:
-    product_lst.append(i)
-
-temp1 = scrap_shopee(search_item,total_of_result)
-for i in temp1:
-    product_lst.append(i)
-
+product_lst.extend(scrap_lazada(search_item,total_of_result))
+product_lst.extend(scrap_shopee(search_item,total_of_result))
     
 df = pd.DataFrame([t.__dict__ for t in product_lst])
 print(df)
