9f7f9f935c1dba4e8875e8f8146af8fa93759a3e
hwen <whman63@gmail.com>
2020-01-06 22:13:06 +0800

    add

--- test2.py
+++ test2.py
@@ -1,8 +1,13 @@
 import requests 
+from classes import Product,Lazada
 from bs4 import BeautifulSoup as soup
 import json
 import pandas as pd
+from selenium import webdriver
 from fake_useragent import UserAgent
+from selenium.webdriver.chrome.options import Options
+
+
 
 my_url = 'https://www.lazada.sg/catalog/?spm=a2o42.home.search.1.64e646b5uVoSRh&q=monitor&_keyori=ss&from=search_history&sugg=monitor_0_1'  
 
@@ -12,17 +17,32 @@ headers = {'User-Agent': userAgent}
 
 
 ret = requests.get(my_url,headers=headers)
-
-
 page_soup = soup(ret.text, 'lxml')
 
 data = page_soup.select("[type='application/ld+json']")[1]
 oJson = json.loads(data.text)["itemListElement"]
-numProducts = len(oJson)
 results = []
 
 for product in oJson:
-    results.append([product['name'], product['offers']['price'], product['image'],product['url']])  # etc......
+    results.append(Lazada(product['name'], product['offers']['price'],"",product['url'], product['image']))  
+    
+#for ratings   it is damn slow!!!
+# webdriver_path= "/usr/local/bin/chromedriver"
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless') 
+# options.add_argument('start-maximized') 
+# options.add_argument('disable-infobars')
+# options.add_argument('--disable-extensions')
 
-df =  pd.DataFrame(results)
+# browser = webdriver.Chrome(webdriver_path,options=options)
+
+# for i in range(len(results)):
+#     browser.get("{}".format(results[i].url))
+#     ratings = browser.find_elements_by_class_name('score-average')
+#     ratings_count = browser.find_elements_by_class_name('count')
+#     num_of_ratings = [i for i in ratings_count[0].text if i.isdigit()==True]
+#     results[i].ratings = ratings[0].text+"("+"".join(num_of_ratings)+")"
+# browser.quit()
+    
+df = pd.DataFrame([t.__dict__ for t in results])
 print(df)
