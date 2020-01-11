f858385d55fa17acaf6644e83b86a8764de86b38
shadow <zac.hong01@gmail.com>
2020-01-11 15:13:12 +0800

    -

--- api.py
+++ api.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from shopee import *
 from test2 import *
 from threading import Thread
-import queue 
+import queue
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 queue = queue.Queue()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -20,16 +21,16 @@ def home():
 @app.route('/search/<string:item>/<int:num>', methods=['GET'])
 def search(item, num):
     try:
-        s = Thread(target=scrap_shopee,args=(item,num,queue))
+        s = Thread(target=scrap_shopee, args=(item, num, queue))
         s.start()
-        l = Thread(target=scrap_lazada ,args=(item,num,queue))
+        l = Thread(target=scrap_lazada, args=(item, num, queue))
         l.start()
 
         result = queue.get()
         s.join()
         l.join()
         result += queue.get()
-        
+
         return render_template("results.html", products=result)
     except Exception:
         return render_template("error.html")
