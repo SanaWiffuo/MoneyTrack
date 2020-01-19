8fd52da3f5a1d85c88feab7a1cb2da728962db54
shadow <zac.hong01@gmail.com>
2020-01-19 13:14:31 +0800

    hello

--- api.py
+++ api.py
@@ -3,8 +3,12 @@ from shopee import *
 from test2 import *
 from threading import Thread
 import queue
+from firebase.firebase import FirebaseApplication
+import requests
 
-app = Flask(__name__)
+url = "https://productify-d0338.firebaseio.com/"
+firebase = FirebaseApplication(url, None)
+app = Flask(__name__, template_folder="templates")
 app.config["DEBUG"] = True
 queue = queue.Queue()
 
@@ -16,6 +20,26 @@ def home():
         num = request.form['num']
         return redirect('/search/{}/{}'.format(item, num))
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form['item']
+        password = request.form['num']
+        users = firebase.get('/users')
+
+    return render_template("login.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form['item']
+        password = request.form['num']
+        firebase.patch("/User/{}".format(username),
+                       {"password": password})
+    return render_template("signup.html")
 
 
 @app.route('/search/<string:item>/<int:num>', methods=['GET'])
