c64cb052ec1f9ccb6362e0484072d4f2f43c9be5
hwen <whman63@gmail.com>
2020-01-26 02:39:51 +0800

    shopee work like a champ

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from shopee import *
 from classes import Shopee
-def product_search(keyword_search):
+def product_search(keyword_search,total_of_result):
     ua = UserAgent()
     userAgent = ua.random
     Shopee_url = 'https://shopee.sg'
@@ -8,12 +8,12 @@ def product_search(keyword_search):
         'User-Agent': userAgent,
         'Referer': '{}search?keyword={}'.format(Shopee_url, keyword_search)
     }
-    url = 'https://shopee.sg/api/v2/search_items/?by=relevancy&keyword={}&limit=50&newest=0&order=desc&page_type=search&version=2'.format(
-        keyword_search)
+    url = 'https://shopee.sg/api/v2/search_items/?by=relevancy&keyword={}&limit={}&newest=0&order=desc&page_type=search&version=2'.format(
+        keyword_search,total_of_result)
     print(url)
-    r = requests.get(url, headers=headers).json()
-    
-    return r
+    r = requests.get(url, headers=headers)
+    print(r.status_code)
+    return r.json()
 
 def product_detail(itemid,shopid):
     ua = UserAgent()
@@ -22,15 +22,18 @@ def product_detail(itemid,shopid):
         'User-Agent': userAgent,
     }
     url = "https://shopee.sg/api/v2/item/get?itemid={}&shopid={}".format(itemid,shopid)
-    r = requests.get(url, headers=headers).json()
+    r = requests.get(url, headers=headers)
+    print(r.request.headers)
+    print(r.status_code)
     
-    return r
+    return r.json()
     
 
 if __name__ == "__main__":
     product_lst = []
-    products = product_search("keyboard")
-    for i in products:
+    products = product_search("monitor",50)
+    # print(products)
+    for i in range(len(products['items'])):  
         itemid = products['items'][i]['itemid']
         shopid = products['items'][i]['shopid']
         item = product_detail(itemid, shopid)
