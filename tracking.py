3fc2fbf8ff3a742a87afb98c5d57fece14e0f00a
hwen <whman63@gmail.com>
2020-02-01 01:12:57 +0800

    rewrite tracking to suit the compute engine

--- tracking.py
+++ tracking.py
@@ -2,37 +2,40 @@ import requests
 from bs4 import BeautifulSoup as soup
 import json
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from fake_useragent import UserAgent
-from selenium.webdriver.chrome.options import Options
 from firebase.firebase import FirebaseApplication
 import time
 from datetime import datetime
 
 def shopee(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless') 
-    options.add_argument('start-maximized') 
-    options.add_argument('disable-infobars')
-    options.add_argument('--disable-extensions')
-    browserdriver = webdriver.Chrome(options = options)
-
-    browserdriver.get(url)
-    time.sleep(2)
-    products = [item for item in WebDriverWait(browserdriver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[type="application/ld+json"]')))]
-    products_json = [product.get_attribute('innerHTML') for product in products[1:]]
+    itemidlst = []
+    shopidlst = []
+    check = 0
+    for i in url[::-1]:
+        if i == ".":
+            check += 1
+        if check == 0:
+            if i.isdigit():
+                itemidlst.append(i)
+        elif check == 1:
+            if i.isdigit():
+                shopidlst.append(i)
+    itemidlst.reverse()
+    itemid = "".join(itemidlst)
+    shopidlst.reverse()
+    shopid = "".join(shopidlst)
+  
+    ua = UserAgent()
+    userAgent = ua.random
+    headers = {
+        'User-Agent': userAgent,
+    }
+    url = "https://shopee.sg/api/v2/item/get?itemid={}&shopid={}".format(itemid,shopid)
+    r = requests.get(url, headers=headers).json()
     
-    for product in products_json:
-        try:
-            price = json.loads(product)['offers']['price']
-        except KeyError:
-            pass
-    
+    price = "$" + str(r['item']['price']/100000)
+        
     return price
-
 
 def lazada(url):
     ua = UserAgent()
@@ -44,9 +47,12 @@ def lazada(url):
 
     ret = requests.get(url,headers=headers)
     page_soup = soup(ret.text, 'lxml')
-
-    data = page_soup.select("[type='application/ld+json']")[0]
+    try:
+        data = page_soup.select("[type='application/ld+json']")[0]
+    except IndexError:
+        return
     price = json.loads(data.text)["offers"]["price"]
+    
     return price
 
 
@@ -59,6 +65,8 @@ if __name__ == "__main__":
     while True:
         result = firebase.get("/", None)
         for name in result:
+            if name == "users":
+                continue
             for i in result[name]:
                 if result[name][i]['platform'] == "Lazada":
                     url = result[name][i]['product url']
@@ -75,5 +83,7 @@ if __name__ == "__main__":
                     st = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
                     firebase.patch("/{}/{}".format(name,i),{"scrape-price":price,"Last-updated":st})
                 time.sleep(120)
+        print("Finished updating")
         time.sleep(1800)
+        
             
