082307bd38c17b0f9326505e34246ef135cd4c3f
Hou Man <54523581+hwennnn@users.noreply.github.com>
2020-10-01 22:49:42 +0800

    Revert "route 404 request"

--- api.py
+++ api.py
@@ -58,8 +58,5 @@ def search(item, num):
     except Exception:
         return render_template("error.html")
 
-@app.errorhandler(404)
-def page_not_found(error):
-   return render_template('error.html', title = '404'), 404
 
 app.run()
